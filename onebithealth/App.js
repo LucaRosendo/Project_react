91f27dc54b694eadb4155e69f9a6eab29bf9f48d
LucaRosendo <lucas115rosendo@gmail.com>
2023-07-14 16:07:31 -0300

    Funcionando

--- onebithealth/App.js
+++ onebithealth/App.js
@@ -1,7 +1,6 @@
 import { StyleSheet, View } from 'react-native';
 import Title from './src/components/Title/index';
-import Main from './src/components/Main/main'//Foi preciso colocar a rota final "/main" porque não está com um nome de "index"
-
+import Main from './src/components/Main/'
 
 export default function App() {
   return (
--- onebithealth/src/components/Form/Index.js
+++ onebithealth/src/components/Form/Index.js
@@ -1,22 +1,57 @@
-import React from "react";
-import { View, Text ,TextInput} from "react-native"
+import React, {useState} from "react";
+import { View, Text ,TextInput, Button} from "react-native"
+import ResultImc from "./ResultImc";
 
 export default function Form(){
+
+const [heigth, setHeigth]= useState(null)//setando estados e criando variáveis
+const [weigth, setWeigth]= useState(null)
+const [messageImc, setMessageImc]= useState("Preencha o peso e altura")
+const [imc, setImc]= useState(null)
+const [textButton, setTextButton]= useState("Calcular")
+
+function imcCalculator(){
+    return setImc((weigth/(heigth*heigth)).toFixed(2))//Função para calcular imc
+}
+
+function validationImc(){//Chama função imcCalculator() quando altura e peso forem diferentes de Null
+    if(weigth != null && heigth != null){
+        imcCalculator()
+        setHeigth(null)
+        setWeigth(null)
+        setMessageImc('Seu imc é igual:')
+        setTextButton("Calcular novamente")
+        return
+    }
+    setImc(null)//else 
+    setTextButton("Calcular")
+    setMessageImc("Preencha peso e altura")
+    
+}
+
     return(
         <View>
             <View>
                 <Text>Altura</Text>
                 <TextInput
-                placeholder="Ex. 1.75"//Exeplo de altura
-                keyboardType="numeric"//Abrir teclado numérico
+                onChangeText = {setHeigth}
+                value = {heigth}
+                placeholder = "Ex. 1.75"//Exeplo de altura
+                keyboardType = "numeric"//Abrir teclado numérico
                 />
 
                 <Text>Peso</Text>
                 <TextInput
-                placeholder="Ex. 75.365"//Exeplo de altura
-                keyboardType="numeric"//Abrir teclado numérico
+                onChangeText = {setWeigth}
+                value = {weigth}
+                placeholder = "Ex. 75.365"//Exeplo de altura
+                keyboardType = "numeric"//Abrir teclado numérico
                 />
+                <Button 
+                onPress = {() => validationImc()}//Quando for pressionado cria uma arrow function para calcular o valor
+                title = {textButton}/>
             </View>
+            <ResultImc messageResultImc={messageImc} resultImc={imc}/>
         </View>
     )
 }
