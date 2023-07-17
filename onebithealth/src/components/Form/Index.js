af2ccb4af3be04c30599bd66f19475cc06f1b759
Lucas Rosendo <104103884+LucaRosendo@users.noreply.github.com>
2023-07-17 10:47:00 -0300

    Commits manuais

--- onebithealth/src/components/Form/Index.js
+++ onebithealth/src/components/Form/Index.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { View, Text ,TextInput, Button} from "react-native";
+import { View, Text ,TextInput, TouchableOpacity} from "react-native";
 import ResultImc from "./ResultImc";
 import styles from "./style";
 
@@ -28,29 +28,37 @@ function validationImc(){//Chama função imcCalculator() quando altura e peso f
     setTextButton("Calcular")
     setMessageImc("Preencha peso e altura")
     
-}
+} 
 
     return(
         <View style={styles.formContext}>
-            <View>
-                <Text>Altura</Text>
+            <View style={styles.form}>
+                <Text style={styles.formLabel}>Altura</Text>
                 <TextInput
+                style={styles.input}
                 onChangeText = {setHeigth}
                 value = {heigth}
                 placeholder = "Ex. 1.75"//Exeplo de altura
                 keyboardType = "numeric"//Abrir teclado numérico
                 />
 
-                <Text>Peso</Text>
+                <Text style={styles.formLabel}>Peso</Text>
                 <TextInput
+                style={styles.input}
                 onChangeText = {setWeigth}
                 value = {weigth}
                 placeholder = "Ex. 75.365"//Exeplo de altura
                 keyboardType = "numeric"//Abrir teclado numérico
                 />
-                <Button 
-                onPress = {() => validationImc()}//Quando for pressionado cria uma arrow function para calcular o valor
-                title = {textButton}/>
+                
+                <TouchableOpacity
+                style={styles.buttonCalculator}
+                onPress={() => {
+                    validationImc()
+                }}>
+                    <Text style={styles.texButtonCalculator}>{textButton}</Text>
+                </TouchableOpacity>
+                    
             </View>
             <ResultImc messageResultImc={messageImc} resultImc={imc}/>
         </View>
--- onebithealth/src/components/Form/style.js
+++ onebithealth/src/components/Form/style.js
@@ -2,12 +2,58 @@ import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
    formContext: {
-    width:"100%",
-    height:"100%",
+    width:"100%",//Largura
+    height:"100%",//Altura
     bottom:0,
     backgroundColor:"#FFFFFF",
     alignItems:"Center",
-    marginTop:30,
+    borderTopLeftRadius:30,//Arredondar bordas
+    borderTopRightRadius:30,//Arredondar bordas
+    marginTop:30,//Separar do top
+   },
+
+   form:{
+      width:"100%",
+      height:"auto",
+      marginTop:39,
+      padding:10,
+   },
+
+   formLabel:{
+   color:"#000000",
+   fontSize:18,
+   paddingLeft:20,
+   },
+
+   input:{
+      width:"90%",
+      borderRadius:50,
+      backgroundColor:"#f6f6f6",
+      height:40,
+      margin:12,
+      paddingLeft:10, 
+   },
+
+   buttonCalculator:{
+      borderRadius:50,
+      alignItems:"center",
+      justifyContent:"center",
+      width:"90%",
+      backgroundColor:"#FF0043",
+      paddingTop:14,
+      paddingBootom:14,
+      marginLeft:12,
+      marginTop:30,
+      paddingVertical: 12,
+      paddingHorizontal: 32
+   },
+
+   texButtonCalculator:{
+      fontSize:20,
+      lineHeight: 21,
+      fontWeight: 'bold',
+      letterSpacing: 0.25,
+      color: 'white',
    },
 });
 
